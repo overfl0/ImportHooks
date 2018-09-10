c8340b38eeb1579d562b54dbc89e7f7e66a021ee
Lukasz Taczuk <overfl0@users.noreply.github.com>
2018-09-10 02:44:54 +0200

    Demo with buttons

--- demo/arduino/arduino.ino
+++ demo/arduino/arduino.ino
@@ -1,5 +1,15 @@
+// https://www.instructables.com/id/Arduino-Button-with-no-resistor/
+// Connect the button between the GND and PIN 12
+// Sending the 'r' command while pressing the button will give you another stream of data back
+// than when the button is not pressed
+
+int buttonPin = 12;
+int LED = 13;
+
 void setup() {
   Serial.begin(115200); // use the same baud-rate as the python side
+  pinMode(buttonPin, INPUT_PULLUP); // Hooray! Free resistors!
+  pinMode(LED, OUTPUT);
 }
 
 void printStuff(const char *text)
@@ -11,11 +21,24 @@ void printStuff(const char *text)
 }
 
 void loop() {
+  int buttonValue = digitalRead(buttonPin);
+  if (buttonValue == LOW){
+    // If button pushed, turn LED on
+    digitalWrite(LED,HIGH);
+  } else {
+    // Otherwise, turn the LED off
+    digitalWrite(LED, LOW);
+  }
+
   if(Serial.available() > 0) {
     char data = Serial.read();
 
     if (data == 'r') {
-      printStuff("raise ImportError(\"Try harder!\")");
+      if (buttonValue == LOW) {
+        printStuff("print(\"Okay, you won! Executing secret stuff!\")");
+      } else {
+        printStuff("raise ImportError(\"Try harder!\")");
+      }
     }
   }
   delay(100);
