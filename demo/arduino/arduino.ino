0093da9d0fc84d35f8bd62f01c5bad798942bdbb
Lukasz Taczuk <overfl0@users.noreply.github.com>
2018-10-14 01:41:31 +0200

    Version for the arduino micro pro

--- demo/arduino/arduino.ino
+++ demo/arduino/arduino.ino
@@ -3,13 +3,18 @@
 // Sending the 'r' command while pressing the button will give you another stream of data back
 // than when the button is not pressed
 
-int buttonPin = 12;
+int buttonPin = 10;
 int LED = 13;
+
+int GREEN_LED = 8;
+int RED_LED = 9;
 
 void setup() {
   Serial.begin(115200); // use the same baud-rate as the python side
   pinMode(buttonPin, INPUT_PULLUP); // Hooray! Free resistors!
   pinMode(LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
 }
 
 void printStuff(const char *text)
@@ -24,10 +29,14 @@ void loop() {
   int buttonValue = digitalRead(buttonPin);
   if (buttonValue == LOW){
     // If button pushed, turn LED on
-    digitalWrite(LED,HIGH);
+    digitalWrite(LED, HIGH);
+    digitalWrite(GREEN_LED, HIGH);
+    digitalWrite(RED_LED, LOW);
   } else {
     // Otherwise, turn the LED off
     digitalWrite(LED, LOW);
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(RED_LED, HIGH);
   }
 
   if(Serial.available() > 0) {
@@ -41,5 +50,5 @@ void loop() {
       }
     }
   }
-  delay(100);
+  delay(50);
 }
